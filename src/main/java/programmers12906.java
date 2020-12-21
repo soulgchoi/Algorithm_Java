c0675024a100c5982972355da22d39999229396e
Solji Choi <bssj9307@gmail.com>
2020-12-21 11:58:12 +0900

    201221 | 같은 숫자는 싫어

---
+++ src/main/java/programmers12906.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class programmers12906 {
+    public static void main(String[] args) {
+        int[] tc1 = { 1,1,3,3,0,1,1 };
+        int[] tc2 = { 4, 4, 4, 3, 3, };
+        solution(tc1);
+        solution(tc2);
+    }
+
+    public static int[] solution(int []arr) {
+        List<Integer> answer = new ArrayList();
+        answer.add(arr[0]);
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] != answer.get(answer.size()-1)) {
+                answer.add(arr[i]);
+            }
+        }
+        System.out.println(answer);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
