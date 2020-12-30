b5ae502a1b9ff63523f45e029b196f4f82402ace
Solji Choi <bssj9307@gmail.com>
2020-12-30 12:27:30 +0900

    201230 | 두 개 뽑아서 더하기

--- src/main/java/programmers68644.java
+++ src/main/java/programmers68644.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class programmers68644 {
     public static void main(String[] args) {
         int[] tc1 = {2, 1, 3, 4, 1};
         int[] tc2 = {5, 0, 2, 7};
-        solution(tc1);
-        solution(tc2);
+        solution2(tc1);
+        solution2(tc2);
     }
 
     public static int[] solution(int[] numbers) {
@@ -20,5 +21,21 @@ public class programmers68644 {
                 .filter(r-> result[r] == 1)
                 .toArray();
         return answer;
+    }
+
+    public static int[] solution2(int[] numbers) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] result = new int[200];
+        for (int i=0; i<numbers.length-1; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                result[numbers[i]+numbers[j]] = 1;
+            }
+        }
+        for (int r=0; r<result.length; r++) {
+            if (result[r] == 1) {
+                answer.add(r);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
